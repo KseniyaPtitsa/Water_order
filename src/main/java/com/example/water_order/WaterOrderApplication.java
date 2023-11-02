54856daa8e416838eb39be71fcb748cfb942da08
bolshovya <bolshov.yv@yandex.ru>
2023-11-02 17:11:30 +0300

    first commit

---
+++ src/main/java/com/example/water_order/WaterOrderApplication.java
@@ -0,0 +1,13 @@
+package com.example.water_order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/water_order/WaterOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.water_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
